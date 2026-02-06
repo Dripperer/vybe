736740422d06670f4590d771d052ddef05aaa340
Dripperer <dripperer@gmail.com>
2026-02-06 22:27:25 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
-document.querySelectorAll("audio").forEach(audio => {
-    audio.volume = 0.7
-})
+const posts = document.querySelectorAll(".post")
+
+let observer = new IntersectionObserver(
+    entries => {
+        entries.forEach(entry => {
+            const audio = entry.target.querySelector("audio")
+            if (!audio) return
+
+            if (entry.isIntersecting) {
+                audio.currentTime = 0
+                audio.play()
+            } else {
+                audio.pause()
+            }
+        })
+    },
+    { threshold: 0.8 }
+)
+
+posts.forEach(post => observer.observe(post))
